900cbcf2ff85c268ad9ea3631e6d86f4884985f7
ShyamSundar-kannujiya <shyamsundar91vivo@gmail.com>
2026-01-25 18:04:56 +0530

    Adjust viewport amount for motion div in Project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,7 +10,7 @@ const Project = () => {
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 1, ease: "easeOut" }}
-      viewport={{ once: false, amount: 0.2 }}
+      viewport={{ once: false, amount: 0.1 }}
       id="projects"
       className="py-20 bg-dark-200"
     >
